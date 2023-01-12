5bbfb0ae6dc93bbbb9c87c2e453b4daad150cd72
KBigForest <gksfka12@naver.com>
2023-01-13 02:21:08 +0900

    HL

--- narsha_project/urls.py
+++ narsha_project/urls.py
@@ -20,7 +20,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('markdownx/', include('markdownx.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
