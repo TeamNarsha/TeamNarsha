1398fb5ba169fe054710f1156e7227a479d3e296
KBigForest <gksfka12@naver.com>
2023-01-13 01:43:36 +0900

    HL

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length = 30)
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
-    name = models.CharField()
+    name = models.CharField(max_length=5)
     
     
     def __str__(self):
