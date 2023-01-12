4b3c1a22b46385a3cc7afa13cbfc71ae5683dfe6
KBigForest <gksfka12@naver.com>
2023-01-12 22:42:18 +0900

    df

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length = 30)
     content = models.TextField()
-    
+    head_image = models.ImageField(upload_to='blog/imagep')
