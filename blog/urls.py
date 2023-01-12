833ce3e924974b5ec7a70e786e78f9d7e6a55b4b
sojung <sojnlee2001@naver.com>
2023-01-13 02:28:40 +0900

    sj

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('<int:value>/', views.narsha_single_pages),
--- narsha_project/settings.py
+++ narsha_project/settings.py
@@ -124,8 +124,10 @@ USE_TZ = False
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-) 
+    os.path.join(BASE_DIR, 'narsha_project', 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
