688fc0ce0d39ba850280e126423fcab6a29d01f5
kimhyuna95 <hyunkr332@gmail.com>
2023-01-12 23:18:12 +0900

    KHA

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,5 @@ class Post(models.Model):
     title = models.CharField(max_length = 30)
     content = models.TextField()
     
+def get_absolute_url(self):
+    return f'/blog/{self.pk}/'
--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,21 @@ from .models import Post
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-pk')
 
     return render(request, 
         'blog/index.html',
         {
             'posts' : posts
         })
+
+def single_post_page(request,value):
+    post=Post.objects.get(pk=value)
+
+    return render(
+        request,
+        'blog/narsha_single_pages.html',
+        {
+        'post' : post,
+        }
+    )
