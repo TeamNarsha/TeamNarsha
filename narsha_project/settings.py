819c89cf60ae4dfc19860262dc7e47a06592d636
KBigForest <gksfka12@naver.com>
2023-01-13 02:52:38 +0900

    HL

--- narsha_project/settings.py
+++ narsha_project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # 사진 삽입을 위한 셋팅
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'blog',
     'single_pages',
+    
 ]
 
 MIDDLEWARE = [
@@ -122,12 +123,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'narsha_project', 'static'),
-)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+    os.path.join(BASE_DIR, 'static'),
+) 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
